83cd62134b0652f5762f2937f4d34b8a9de43381
Rafał Szypulewski <rafalszypulewski@gmail.com>
2017-06-22 16:07:39 +0200

    Add base webdriver class and users class

---
+++ Automation/Automation/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation
+{
+    class Users
+    {
+        public class User
+        {
+            public string Login { get; set; }
+            public string Password { get; set; }
+            public User(string email, string password)
+            {
+                this.Login = email;
+                this.Password = password;
+            }
+        }
+
+        public static User Admin = new User("administrator", "qwerty");
+        public static User VO = new User("vo", "qwerty");
+    }
+}
